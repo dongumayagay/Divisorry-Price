60d2cabfddf12a59855ebc79038eb87349775c2a
Don Gumayagay <dngumayagay@gmail.com>
2021-11-15 18:37:12 +0800

    change config file to use vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
