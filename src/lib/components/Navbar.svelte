3119f55f7e76f7a247c81ec06eb7ef7670d49034
Don Gumayagay <dngumayagay@gmail.com>
2022-02-04 14:06:00 +0800

    add account icon

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -18,10 +18,26 @@
 				alt="brand logo"
 			/>
 		</a>
+		<button aria-label="account"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 lg:h-9 hover:text-yellow-500 transition-colors duration-500"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1.5"
+					d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+				/>
+			</svg></button
+		>
 		<button aria-label="search" on:click={() => (showSearchbar = true)}
 			><svg
 				xmlns="http://www.w3.org/2000/svg"
-				class="h-7 lg:h-9 hover:text-yellow-500 transition-colors"
+				class="h-6 lg:h-9 hover:text-yellow-500 transition-colors duration-500"
 				fill="none"
 				viewBox="0 0 24 24"
 				stroke="currentColor"
@@ -37,7 +53,7 @@
 		<button aria-label="show cart" on:click={() => (showCart = true)}
 			><svg
 				xmlns="http://www.w3.org/2000/svg"
-				class="h-7 lg:h-9 hover:text-yellow-500 transition-colors"
+				class="h-6 lg:h-9 hover:text-yellow-500 transition-colors duration-500"
 				fill="none"
 				viewBox="0 0 24 24"
 				stroke="currentColor"
