a31accd6c9d59fb713238fbd705254c38257e8bd
Don Gumayagay <dngumayagay@gmail.com>
2022-02-09 07:26:23 +0800

    temporarily disable search button

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -33,7 +33,7 @@
 				/>
 			</svg></a
 		>
-		<button aria-label="search" on:click={() => (showSearchbar = true)}
+		<!-- <button aria-label="search" on:click={() => (showSearchbar = true)}
 			><svg
 				xmlns="http://www.w3.org/2000/svg"
 				class="h-6 lg:h-7 hover:text-yellow-500 transition-colors duration-500 stroke-[1.5] lg:stroke-2"
@@ -47,7 +47,7 @@
 					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
 				/>
 			</svg></button
-		>
+		> -->
 		<button aria-label="show cart" on:click={() => ($showCart = true)}
 			><svg
 				xmlns="http://www.w3.org/2000/svg"
