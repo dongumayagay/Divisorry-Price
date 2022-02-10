dfbb412aa0bd535bdbd70dbb2aaa248e3b84e93c
Don Gumayagay <dngumayagay@gmail.com>
2022-02-10 08:44:06 +0800

    update about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,55 +1,73 @@
 <script context="module">
-  export const prerender = true;
+	export const prerender = true;
+</script>
+
+<script>
+	const links = [
+		{
+			title: 'Linus Tech Tips Store',
+			link: 'https://www.lttstore.com/'
+		},
+		{ title: 'MKBHD', link: 'https://mkbhd.com/' },
+		{
+			title: 'MrBeast | Official Store',
+			link: 'https://shopmrbeast.com/'
+		},
+		{
+			title: 'PewDiePie // Store',
+			link: 'https://represent.com/store/pewdiepie'
+		},
+		{
+			title: 'Peak Design',
+			link: 'https://www.peakdesign.com/'
+		},
+		{
+			title: 'Wandrd',
+			link: 'https://wandrd-pacific.com/'
+		}
+	];
 </script>
 
 <svelte:head><title>Divisorry Price - About</title></svelte:head>
-<main class="container mx-auto p-8">
-  <h1 class="text-center text-4xl font-bold">About</h1>
-  <p class="text-lg my-4">
-    Tart gummies marzipan dragée dragée cake brownie. Candy shortbread gummies
-    wafer gingerbread. Tiramisu wafer icing sweet roll bonbon brownie toffee pie
-    marzipan. Sweet chocolate bar pudding gingerbread powder pie. Fruitcake
-    lemon drops macaroon sweet roll brownie cake pie. Tiramisu pudding brownie
-    dessert pudding caramels topping bear claw. Shortbread liquorice sugar plum
-    brownie lollipop dragée chocolate cake. Cake cupcake wafer bonbon soufflé
-    caramels. Macaroon chocolate brownie cotton candy apple pie chocolate bar.
-    Topping ice cream cake chocolate cake chupa chups ice cream biscuit wafer.
-  </p>
-  <p class="text-lg my-4">
-    Fruitcake wafer cake brownie croissant sweet. Oat cake ice cream gummi bears
-    sweet roll shortbread cotton candy. Pastry sweet roll pudding toffee pie
-    dragée cupcake. Wafer marzipan apple pie soufflé chocolate cake liquorice
-    lollipop. Marshmallow lollipop liquorice candy canes sweet roll fruitcake
-    jelly beans candy canes cupcake. Jelly-o jelly beans topping topping brownie
-    chocolate bar apple pie donut.
-  </p>
-  <p class="text-lg my-4">
-    Marshmallow jelly-o dragée candy chupa chups ice cream. Sugar plum jujubes
-    brownie carrot cake liquorice liquorice bear claw dragée gingerbread. Icing
-    gingerbread wafer croissant tiramisu cheesecake candy canes. Icing sesame
-    snaps tiramisu toffee danish powder. Halvah tart jelly dragée soufflé cake
-    ice cream sesame snaps muffin. Sweet roll oat cake tart donut pastry wafer
-    pudding. Ice cream fruitcake soufflé caramels cake dessert ice cream bonbon
-    tart.
-  </p>
-  <p class="text-lg my-4">
-    Chupa chups dessert marzipan donut halvah powder. Cake danish jujubes
-    topping jelly beans marzipan dessert pudding. Wafer jelly-o biscuit cookie
-    oat cake lemon drops fruitcake sweet roll cotton candy. Candy canes danish
-    bear claw jujubes pastry. Cake cake danish tart gummies tootsie roll danish.
-    Gingerbread chocolate bar chocolate cake toffee ice cream. Caramels
-    fruitcake sugar plum soufflé cookie liquorice. Marshmallow jelly-o gummi
-    bears brownie wafer cotton candy bear claw. Oat cake oat cake brownie tart
-    toffee. Cookie gummies shortbread pastry lollipop lemon drops jujubes donut
-    jelly.
-  </p>
-  <p class="text-lg my-4">
-    Tiramisu lollipop jelly-o tootsie roll muffin biscuit. Cotton candy pastry
-    icing carrot cake gingerbread lemon drops dragée dragée cookie. Croissant
-    tiramisu danish jelly pie bear claw chocolate cake. Fruitcake bonbon cookie
-    sugar plum cake cupcake. Powder marshmallow muffin caramels sweet roll.
-    Pudding gummi bears sugar plum cupcake gummies cookie. Bear claw pie tootsie
-    roll brownie chocolate. Donut chocolate cake tootsie roll tart biscuit jelly
-    beans. Oat cake oat cake sweet roll donut danish ice cream.
-  </p>
-</main>
+<article class="container mx-auto p-8">
+	<h1 class="text-center text-4xl font-bold">About</h1>
+	<p class="py-2 text-lg">
+		Ecommerce Project that uses <a
+			class="border-b border-black font-medium"
+			href="https://kit.svelte.dev/">Sveltekit</a
+		>
+		for the Front-end and SSR, and
+		<a class="border-b border-black font-medium" href="https://tailwindcss.com/">TailwindCSS</a> for
+		styling.
+	</p>
+	<p class="py-2 text-lg">
+		Products used in the project is from <a
+			class="border-b border-black font-medium"
+			href="https://fakestoreapi.com/">FakeStoreAPI</a
+		>, I transfer products to my own API for faster response time.
+	</p>
+	<h1 class="pt-2 text-lg">Design Insipirations</h1>
+	<p class="text-lg px-4">
+		Since I'm not good at design. here is some sites that i looked up for some design inspirations.
+	</p>
+	<ul class="px-8">
+		{#each links as item}
+			<li class="list-disc">
+				<a
+					target="_blank"
+					rel="noreferrer noopener"
+					class="font-medium border-b border-black"
+					href={item.link}>{item.title}</a
+				>
+			</li>
+		{/each}
+	</ul>
+	<h1 class="py-4">
+		Source Code : <a
+			class="font-medium"
+			target="_blank"
+			rel="noreferrer noopener"
+			href="https://github.com/dongumayagay/Divisorry-Price">Here</a
+		>
+	</h1>
+</article>
