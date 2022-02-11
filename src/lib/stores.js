debbd243c6e901564690b2337813a645378f8af4
Don Gumayagay <dngumayagay@gmail.com>
2022-02-11 21:51:15 +0800

    cart persistance update

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,7 +1,9 @@
 import { writable } from 'svelte/store';
-
-export let showCart = writable(false);
-
-export let cart = writable([]);
+import { browser } from '$app/env';
 
 export let session = writable();
+export let showCart = writable(false);
+
+const storedcart = JSON.parse(browser && localStorage.getItem('cart')) || [];
+export const cart = writable(browser && storedcart);
+cart.subscribe((val) => browser && (localStorage.cart = JSON.stringify(val)));
