b61251b6e672b6ae384736fb83ef2985d51595ba
Don Gumayagay <dngumayagay@gmail.com>
2022-02-19 18:40:53 +0800

    hide ui when there is no session

--- src/routes/account.svelte
+++ src/routes/account.svelte
@@ -45,19 +45,21 @@
 
 <svelte:head><title>Your Account | Divisorry Price</title></svelte:head>
 
-<main class="container mx-auto py-8 px-4 space-y-4">
-	<header class="flex justify-between items-center">
-		<span class="uppercase font-semibold text-2xl"> Your Account </span>
-		<button on:click={logout} class="capitalize bg-neutral-800 text-white px-4 py-2 rounded-full"
-			>logout</button
-		>
-	</header>
-	<h1 class="text-xl sm:text-3xl py-4 text-center font-medium">
-		Kamusta Suking, <span class="capitalize text-yellow-500">
-			{userDetails ? userDetails.firstName + '!' : '. . .'}
-		</span>
-	</h1>
+{#if $session}
+	<main class="container mx-auto py-8 px-4 space-y-4">
+		<header class="flex justify-between items-center">
+			<span class="uppercase font-semibold text-2xl"> Your Account </span>
+			<button on:click={logout} class="capitalize bg-neutral-800 text-white px-4 py-2 rounded-full"
+				>logout</button
+			>
+		</header>
+		<h1 class="text-xl sm:text-3xl py-4 text-center font-medium">
+			Kamusta Suking, <span class="capitalize text-yellow-500">
+				{userDetails ? userDetails.firstName + '!' : '. . .'}
+			</span>
+		</h1>
 
-	<UserDetails {userDetails} {userInfoDocRef} />
-	<YourOrders />
-</main>
+		<UserDetails {userDetails} {userInfoDocRef} />
+		<YourOrders />
+	</main>
+{/if}
