471e5566de3ac78c110e3d18848aab28d0b23d4e
Don Gumayagay <dngumayagay@gmail.com>
2022-02-21 08:44:34 +0800

    put error indicator on the form

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -30,94 +30,84 @@
 		} catch (error) {
 			console.log(error.code);
 			errorMessage = formatErrorCode(error.code);
-
-			// alert(formatErrorCode(error.code));
-			// switch (error.code) {
-			// 	case 'auth/user-not-found':
-			// 		errorMessage = "User doesn't exist";
-			// 		break;
-
-			// 	default:
-			// 		errorMessage = formatErrorCode(error.code);
-			// 		break;
-			// }
 		}
 	}
 </script>
 
 <svelte:head><title>Login | Divisorry Price</title></svelte:head>
-
-<div class="flex-1 flex justify-center items-center">
-	<main
-		class="flex flex-col bg-white border border-neutral-300/75 shadow-xl w-full sm:w-[450px] py-12 px-6 rounded-xl"
-	>
-		<h1 class="uppercase font-bold tracking-widest text-3xl pb-4 text-center">
-			Login into your Account
-		</h1>
-		<form class="flex flex-col" on:submit|preventDefault={submitHandler}>
-			<label class="pb-2">
-				<span class="inline-block text-sm pl-4"> Email </span>
-				<input
-					on:focus={() => (errorMessage = null)}
-					required
-					name="email"
-					class="w-full rounded-full px-4 text-lg"
-					type="email"
-					placeholder="Email"
-				/>
-			</label>
-			<label class="pb-6">
-				<span class="inline-block text-sm pl-4"> Password </span>
-				<input
-					on:focus={() => (errorMessage = null)}
-					required
-					name="password"
-					class="w-full rounded-full px-4 text-lg"
-					type="password"
-					placeholder="Password"
-				/>
-			</label>
-			{#if errorMessage}
-				<p class="mb-4 text-center text-red-500">{errorMessage}</p>
-			{/if}
-			<button
-				class="grid grid-cols-3 items-center  bg-neutral-900 rounded-full px-4 py-3 capitalize tracking-widest text-lg font-medium text-white"
-				type="submit"
-			>
-				<span class="col-span-1" />
-				<span class="col-span-1 "> login </span>
-				<span class="col-span-1 flex justify-center">
-					{#if $navigating}
-						<svg
-							class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-						>
-							<circle
-								class="opacity-25"
-								cx="12"
-								cy="12"
-								r="10"
-								stroke="currentColor"
-								stroke-width="4"
-							/>
-							<path
-								class="opacity-75"
-								fill="currentColor"
-								d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-							/>
-						</svg>
-					{/if}
-				</span>
-			</button>
-			<a
-				sveltekit:prefetch
-				href="/signup"
-				class="mx-auto text-center transition pt-4 text-lg text-neutral-600 w-fit border-transparent border-b hover:border-black"
-			>
-				Don't have an account? Create one
-			</a>
-		</form>
-	</main>
-</div>
+{#if !$session}
+	<div class="flex-1 flex justify-center items-center">
+		<main
+			class="flex flex-col bg-white border border-neutral-300/75 shadow-xl w-full sm:w-[450px] py-12 px-6 rounded-xl"
+		>
+			<h1 class="uppercase font-bold tracking-widest text-3xl pb-4 text-center">
+				Login into your Account
+			</h1>
+			<form class="flex flex-col" on:submit|preventDefault={submitHandler}>
+				<label class="pb-2">
+					<span class="inline-block text-sm pl-4"> Email </span>
+					<input
+						on:focus={() => (errorMessage = null)}
+						required
+						name="email"
+						class="w-full rounded-full px-4 text-lg"
+						type="email"
+						placeholder="Email"
+					/>
+				</label>
+				<label class="pb-6">
+					<span class="inline-block text-sm pl-4"> Password </span>
+					<input
+						on:focus={() => (errorMessage = null)}
+						required
+						name="password"
+						class="w-full rounded-full px-4 text-lg"
+						type="password"
+						placeholder="Password"
+					/>
+				</label>
+				{#if errorMessage}
+					<p class="mb-4 text-center text-red-500">{errorMessage}</p>
+				{/if}
+				<button
+					class="grid grid-cols-3 items-center  bg-neutral-900 rounded-full px-4 py-3 capitalize tracking-widest text-lg font-medium text-white"
+					type="submit"
+				>
+					<span class="col-span-1" />
+					<span class="col-span-1 "> login </span>
+					<span class="col-span-1 flex justify-center">
+						{#if $navigating}
+							<svg
+								class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+							>
+								<circle
+									class="opacity-25"
+									cx="12"
+									cy="12"
+									r="10"
+									stroke="currentColor"
+									stroke-width="4"
+								/>
+								<path
+									class="opacity-75"
+									fill="currentColor"
+									d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+								/>
+							</svg>
+						{/if}
+					</span>
+				</button>
+				<a
+					sveltekit:prefetch
+					href="/signup"
+					class="mx-auto text-center transition pt-4 text-lg text-neutral-600 w-fit border-transparent border-b hover:border-black"
+				>
+					Don't have an account? Create one
+				</a>
+			</form>
+		</main>
+	</div>
+{/if}
